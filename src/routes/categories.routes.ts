67172b65a9602f586efd3d64a7cfb64f85dc988d
Thomas Lossio <thomaslossio@hotmail.com>
2022-04-11 19:25:19 -0300

    Add Category's model, route and repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,19 @@
 import { Router } from "express";
 
+import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
+const categories: Category[] = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
-    name,
-    description,
-  });
+  categoriesRepository.create({ name, description });
 
-  return response.status(201).send();
+  return response.status(201).json();
 });
 
 export { categoriesRoutes };
