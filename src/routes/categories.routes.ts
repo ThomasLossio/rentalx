a58e2461cbef66c6e0ea84d1d63bc5bc29f83d77
Thomas Lossio <thomaslossio@hotmail.com>
2022-04-11 19:26:50 -0300

    remove unusual declaration variable

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
-
-const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
